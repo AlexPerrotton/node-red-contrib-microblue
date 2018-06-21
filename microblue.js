b24f81ed71996e9f01792495a9b183a9e4520800
AlexPerrotton <39795143+AlexPerrotton@users.noreply.github.com>
2018-06-21 13:57:14 +0200

    Update microblue.js

--- microblue.js
+++ microblue.js
@@ -47,8 +47,82 @@ var microblue = require('microblue');
                 });
             });
         }
-
+	
         RED.nodes.registerType("WriteUart", WriteUart);
+	    
+	function ReadAccel(n){
+            RED.nodes.createNode(this,n);
+            var node = this;
+            this.name = n.name;
+		
+	    microblue.connect(function(){
+            node.status({fill:"blue", shape:"dot", text:"Connect"});
+            var intervalFunc = setInterval(function(){
+                microblue.accelerometer(function(data){
+                    node.send({topic:"micro:bit/"+node.name, payload:data});
+                });
+            }, 500);
+            });
+
+            node.on("close", function(done){
+                node.status({fill:"grey", shape:"dot", text:"Disconnect"});
+                this.sensor(function(){
+                    done();
+                });
+            });
+        }
+
+        RED.nodes.registerType("ReadAccel", ReadAccel);
+
+        function ReadCompass(n){
+            RED.nodes.createNode(this,n);
+            var node = this;
+            this.name = n.name;
+
+            microblue.connect(function(){
+                node.status({fill:"blue", shape:"dot", text:"Connect"});
+                var intervalFunc = setInterval(function(){
+                    microblue.compass(function(data){
+                        node.send({topic:"micro:bit/"+node.name, payload:data});
+                    });
+                }, 500);
+            });
+
+            node.on("close", function(done){
+                node.status({fill:"grey", shape:"dot", text:"Disconnect"});
+                this.sensor(function(){
+                    done();
+                });
+            });
+        }
+
+        RED.nodes.registerType("ReadCompass", ReadCompass);
+	    
+	function Compass_Accel(n){
+            RED.nodes.createNode(this,n);
+            var node = this;
+            this.name = n.name;
+
+            microblue.connect(function(){
+                node.status({fill:"blue", shape:"dot", text:"Connect"});
+                var intervalFunc = setInterval(function(){
+                    microblue.compass(function(data_compass){
+                        microblue.accelerometer(function(data_accel){
+                                node.send({topic:"micro:bit/"+node.name, payload:{"Compass":[data_compass],"Accelerometer":[data_accel]}});
+                        });
+                    });
+                }, 500);
+            });
+
+            node.on("close", function(done){
+                node.status({fill:"grey", shape:"dot", text:"Disconnect"});
+                this.sensor(function(){
+                    done();
+                });
+            });
+        }
+
+        RED.nodes.registerType("Compass_Accel", Compass_Accel);
 
     }
 
